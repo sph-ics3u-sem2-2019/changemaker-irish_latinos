aea0bd48bc1011a1efa4ecd89701b0797afd4acf
jeancarlos <jeancarlos.herrera@ocsbstudent.ca>
2019-02-27 09:55:44 -0500

    wednesday work

--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,21 @@ public class Main {
 		System.out.println("How much was paid?");
 		double payment=in.nextDouble();
 		//TODO: Here is where your code goes
+		double change=payment-cost;
+		int num100s;
+		int num50s;
+		int num20s;
+		int num10s;
+		//How many 100 dollar bills?
+		System.out.println("your change is "+change);
+		num100s=(int)(change/100);
+		//Find out how much money is left
+		change=change%100;
+		//how many 50s?
+		
+		System.out.println(num100s+" x $100");
+				
+		
 	}
 
 }
